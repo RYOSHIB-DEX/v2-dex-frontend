5da16d963ca7bb716ad154b33f23956bfc554129
FrankFourierX <francescofilippotandoi@gmail.com>
2023-09-10 06:53:28 +0700

    Update APR.jsx

--- src/integratedFarms/components/FarmsCard/APR.jsx
+++ src/integratedFarms/components/FarmsCard/APR.jsx
@@ -9,11 +9,7 @@ const APR = () => {
         <div className="farmAprValues flex flex-row justify-center gap-4 items-center">
           <span className="flex gap-2 text-[#00ffff] custfont justify-center items-center">
             <IoRocket className="text-[#00ffff] " />
-            Up to48.41%
-          </span>
-          <span className="flex gap-2 items-center text-white">
-            <s className="border-b-2 border-dotted custfont ">24.58%</s>
-            {/* <AiFillCalculator className="text-purple" /> */}
+            Up to 5%
           </span>
         </div>
       </div>
