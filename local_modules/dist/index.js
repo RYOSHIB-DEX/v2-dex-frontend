ab3206fe3dd259dd72aaa5ece7613c2cb24b277d
FrankFourierX <francescofilippotandoi@gmail.com>
2023-09-10 06:31:44 +0700

    update

--- local_modules/dist/index.js
+++ local_modules/dist/index.js
@@ -1,8 +1,3 @@
-
 'use strict'
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./uniswap-v2-sdk.cjs.production.min.js')
-} else {
-  module.exports = require('./uniswap-v2-sdk.cjs.development.js')
-}
+module.exports = require('./uniswap-v2-sdk.cjs.development.js')
